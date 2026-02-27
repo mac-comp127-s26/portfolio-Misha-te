485daf28c0312c2c41b3f93ee3a408cb062c114d
Ngina-Misha Awan <mawan@macalester.edu>
2026-02-27 16:45:32 -0600

    Extras

---
+++ src/AreaOfSquare.java
@@ -0,0 +1,10 @@
+public class AreaOfSquare {
+    public static void main (String[]args){
+        //length/dimensions
+        double side = 14;
+        //area
+        double area;
+        area= side*side;
+        System.out.println("Dear User, the area of your square is: "+area+ " cm squared");
+    }
+}
--- src/exam/Exam1.java
+++ src/exam/Exam1.java
@@ -4,5 +4,9 @@ package exam;
 public class Exam1 {
     public static void main(String[] args) {
         System.out.println("Hello from Exam1 in exam");
+         double x = 20;
+         double y = 24;
+         double average =(x+y)/2;
+        System.out.println(average);
     }
 }
