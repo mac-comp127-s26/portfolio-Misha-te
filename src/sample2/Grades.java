b03f10b13cf7270013908d6d70dec4e81cbb18f0
Ngina-Misha Awan <mawan@macalester.edu>
2026-04-26 20:58:53 -0500

    Removed Scanners in Getters

--- src/sample2/Grades.java
+++ src/sample2/Grades.java
@@ -7,52 +7,57 @@ public class Grades {
     private double midTerm2;
     private double finalExam;
     private double extra;
-    public String student;
+    //public String student;
 
     public Grades(double midTerm1,double midTerm2, double finalExam, double extra){
         this.extra=extra;
         this.finalExam=finalExam;
         this.midTerm1=midTerm1;
-        this.midTerm1=midTerm1;
+        this.midTerm2=midTerm2;
+    }
+   
     }
     public double getMidterm1(){
-        while (true){
-            Scanner scanner1 =new Scanner(System.in);
-            System.out.println("Please enter your midterm 1 score: ");
-            double midterm1 = scanner1.nextDouble();
-            if (midterm1 >=0 && midterm1>=100){
-                break;
-            }else{
-                System.out.println("Enter a value between 0 and 100 :");
-            }
-         }
-        return midTerm1;     
+            return midTerm1;     
     }
     public double getMidterm2(){
-        Scanner scanner2 =new Scanner(System.in);
-        System.out.println("Please enter your midterm 2 score: ");
-        double midterm2 = scanner2.nextDouble();
-        return midterm2;
+        return midTerm2;
     }
     public double getFinalExam(){
-        Scanner scanner3 =new Scanner(System.in);
-        System.out.println("Please enter your final score: ");
-        double finalExam = scanner3.nextDouble();
         return finalExam;
     }
     public double getExtra(){
-        Scanner scanner4 =new Scanner(System.in);
-        System.out.println("Enter Extra Credit: ");
-        double extra = scanner4.nextDouble();
         return extra;
     
     }
     
-    }
+    
     public static void main(String[] args) {
-        System.out.println("HelloYOUTHERE");
+        System.out.println("Hello YOUTHE RE");
         //double  m= new getMidterm1();
+        Scanner sc = new Scanner(System.in);
         
+        System.out.print("Midterm 1: ");
+        double m1 = sc.nextDouble();
+
+        System.out.print("Midterm 2: ");
+        double m2 = sc.nextDouble();
+
+        System.out.print("Final Exam: ");
+        double fin = sc.nextDouble();
+
+        System.out.print("Enter midterm 1");
+        double ext = sc.nextDouble();
+
+        Grades grades = new Grades(m1,m2,fin,ext);
+
+
+        
+
+        double finalGrade = (0.2*grades.getMidterm1())+(0.3*grades.getMidterm1())+(0.5*grades.getFinalExam())+grades.getExtra();
+        System.out.println(finalGrade);
+
+    
         //System.out.println(james.getMidterm1());
         
     }
