1d452d0d198015689de7eabf316d137033f8b871
Ngina-Misha Awan <mawan@macalester.edu>
2026-05-01 19:59:54 -0500

    Updated expense

--- src/sample2/ExpenseTracker.java
+++ src/sample2/ExpenseTracker.java
@@ -1,5 +1,46 @@
 package sample2;
 
 public class ExpenseTracker {
-    
-}
+    private String expense;
+    private double amount;
+    private String description;
+    private char category;
+    private String date;
+    private double expenditure;
+
+    public ExpenseTracker(String expense, double amount,String description, char category,String date, double expenditure){
+        this.expense= expense;
+        this.amount = amount;
+        this.description= description;
+        this.date= date;
+        this.category =category;
+        this.expenditure = expenditure;
+
+    }
+    public String getExpense(){
+        return expense;
+    }
+    public double getAmount(){
+        return amount;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public String getDate(){
+        return date;
+    }
+    public char  getCategory(){
+        //L- Luxury
+        //B-Basic if in ??
+        //Utilities- 
+        return category;
+    }
+    public double getExpenditure(){
+        return expenditure;
+    }
+    @Override
+    public String toString(){
+        return "Expense(date=" + date + ", description=" + description + ", amount=" + amount + ", category=" + category + ")";
+    }
+    }
+
--- src/sample2/Hello.java
+++
@@ -1,7 +0,0 @@
-package sample2;
-
-public class Hello {
-    public static void main(String[]args){
-        System.out.println("Hello Misha, you good?");
-    }
-}
